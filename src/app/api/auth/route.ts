4bbf05e29c0fcef4389774c146d6dbe886202ed0
Divine Lewis <divinitysoft0@gmail.com>
2026-02-28 18:51:29 +0100

    capital letter changes

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import Admin from "@/models/admin";
+import Admin from "@/models/Admin";
 import { connectDB } from "@/lib/db";
 
 export async function POST(req: Request) {
